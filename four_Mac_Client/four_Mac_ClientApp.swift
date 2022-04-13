d09a62bb63c51a3d9ef6e2a77a885fe089517600
mhahn <mhahn@student.42heilbronn.de>
2022-04-13 11:49:36 +0200

    Initial Commit

---
+++ four_Mac_Client/four_Mac_ClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  four_Mac_ClientApp.swift
+//  four_Mac_Client
+//
+//  Created by Manuel Hahn on 4/13/22.
+//
+
+import SwiftUI
+
+@main
+struct four_Mac_ClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
