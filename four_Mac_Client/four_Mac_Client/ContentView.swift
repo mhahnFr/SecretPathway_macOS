dfa105a810b22e8636c840cad69aa3007c01ff45
mhahn <mhahn@student.42heilbronn.de>
2022-04-21 16:33:50 +0200

    Readded everything

---
+++ four_Mac_Client/four_Mac_Client/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  four_Mac_Client
+//
+//  Created by Manuel Hahn on 4/21/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
