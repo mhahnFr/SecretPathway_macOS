9b7c24fc54b4d2124ad330bce213f9de9803f1c6
mhahn <mhahn@student.42heilbronn.de>
2022-06-10 15:10:12 +0200

    The text can now be multilined

--- four_Mac_Client/four_Mac_Client/ContentView.swift
+++ four_Mac_Client/four_Mac_Client/ContentView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         Text("Hello, World!")
+            .fixedSize(horizontal: false, vertical: true)
             .frame(maxWidth: .infinity, maxHeight: .infinity)
     }
 }
