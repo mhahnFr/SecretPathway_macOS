1661902047b176a457a5b90bdb514c806cfbe177
mhahnFr <83553794+mhahnFr@users.noreply.github.com>
2022-10-22 17:47:48 +0200

    Added settings menu item action

--- src/AppDelegate.swift
+++ src/AppDelegate.swift
@@ -26,6 +26,10 @@ import SwiftUI
 class AppDelegate: NSObject, NSApplicationDelegate {
     var window: NSWindow!
     var connection: ClientConnection!
+    
+    @IBAction func settingsAction(_ sender: NSMenuItem) {
+        print("TODO: Open settings window")
+    }
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Create the SwiftUI view that provides the window contents.
