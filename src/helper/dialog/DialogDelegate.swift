f30aebf3b86fbc77c5f816102595798e1d225b07
mhahnFr <83553794+mhahnFr@users.noreply.github.com>
2022-11-15 18:33:14 +0100

    Documented the Dialog's delegate

--- src/helper/dialog/DialogDelegate.swift
+++ src/helper/dialog/DialogDelegate.swift
@@ -21,40 +21,55 @@
 
 import AppKit
 
+/// The delegate class for a modal dialog.
 class DialogDelegate: NSObject, NSWindowDelegate, ObservableObject {
-    
+    /// The main message displayed by the view.
     @Published var message  = ""
+    /// The additional text.
     @Published var addition = ""
+    /// The text label of the accept button. Not displayed if nil.
     @Published var acceptButton: String?
+    /// The text label for the dismiss button. Not displayed if nil.
     @Published var dismissButton: String?
+    /// Other buttons that are displayed between the dismiss button and the accept button.
     @Published var otherButtons: [String] = []
     
+    /// Indicates whether the user has pressed the accept button.
     private(set) var accepted = false
+    /// The button the user clicked on.
     private(set) var usedButton: String?
     
+    /// The window this delegate controls.
     private weak var window: NSWindow?
     
+    /// Initializes this instance for the given window.
+    ///
+    /// - Parameter window: The window this instance should control.
     init(for window: NSWindow?) {
         self.window = window
     }
     
+    /// Marks this instance as accepted and closes the associated window.
     func accept() {
         accepted = true
         usedButton = acceptButton
         close()
     }
     
+    /// Marks this instance as not accepted and closes the associated window.
     func dismiss() {
         accepted = false
         usedButton = dismissButton
         close()
     }
     
+    /// Sets the used button to the given one and closes the associated window.
     func action(for button: String) {
         usedButton = button
         close()
     }
     
+    /// Closes the associated window.
     func close() {
         window?.close()
     }
