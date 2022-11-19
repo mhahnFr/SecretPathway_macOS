410ba014f1f12fa941b122ea6e083acc21d4105c
mhahnFr <83553794+mhahnFr@users.noreply.github.com>
2022-11-19 19:55:19 +0100

    Added basic AppKit <-> SwiftUI bridge

---
+++ src/gui/bridge/NSTextViewBridge.swift
@@ -0,0 +1,42 @@
+/*
+ * SecretPathway_macOS - A MUD client, for macOS.
+ *
+ * Copyright (C) 2022  mhahnFr
+ *
+ * This file is part of the SecretPathway_macOS. This program is free
+ * software: you can redistribute it and/or modify it under the terms
+ * of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program, see the file LICENSE.
+ * If not, see <https://www.gnu.org/licenses/>.
+ */
+
+import AppKit
+import SwiftUI
+
+struct NSTextViewBridge: NSViewRepresentable {
+    var text: String
+    var fontSize: Double
+    
+    func makeNSView(context: Context) -> some NSView {
+        let textView = NSTextView()
+        textView.isEditable = false
+        return textView
+    }
+    
+    func updateNSView(_ nsView: NSViewType, context: Context) {
+        let textView = nsView as! NSTextView
+        
+        textView.textStorage?.setAttributedString(NSAttributedString(string: text))
+        textView.font = NSFont.monospacedSystemFont(ofSize: fontSize, weight: .regular)
+        textView.textColor = .white
+    }
+}
