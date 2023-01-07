6765ac0d2cf56b6f26881960f381400301764003
mhahnFr <83553794+mhahnFr@users.noreply.github.com>
2023-01-07 17:55:48 +0100

    Refactored the tokenizer, part II

--- src/core/parser/tokenizer/Tokenizer.swift
+++ src/core/parser/tokenizer/Tokenizer.swift
@@ -52,99 +52,53 @@ struct Tokenizer {
             } else {
                 return nextToken()
             }
-        } else if stream.peek("(") {
-            return Token(begin: stream.index, type: .leftParen, end: stream.skip())
-        } else if stream.peek(")") {
-            return Token(begin: stream.index, type: .rightParen, end: stream.skip())
-        } else if stream.peek("[") {
-            return Token(begin: stream.index, type: .leftBrack, end: stream.skip())
-        } else if stream.peek("]") {
-            return Token(begin: stream.index, type: .rightBrack, end: stream.skip())
-        } else if stream.peek("{") {
-            return Token(begin: stream.index, type: .leftCurly, end: stream.skip())
-        } else if stream.peek("}") {
-            return Token(begin: stream.index, type: .rightCurly, end: stream.skip())
-        } else if stream.peek("...") {
-            return Token(begin: stream.index, type: .ellipsis, end: stream.skip(3))
-        } else if stream.peek(".") {
-            return Token(begin: stream.index, type: .dot, end: stream.skip())
-        } else if stream.peek(",") {
-            return Token(begin: stream.index, type: .comma, end: stream.skip())
-        } else if stream.peek("::") {
-            return Token(begin: stream.index, type: .scope, end: stream.skip(2))
-        } else if stream.peek(":") {
-            return Token(begin: stream.index, type: .colon, end: stream.skip())
-        } else if stream.peek(";") {
-            return Token(begin: stream.index, type: .semicolon, end: stream.skip())
-        } else if stream.peek("==") {
-            return Token(begin: stream.index, type: .equals, end: stream.skip(2))
-        } else if stream.peek("!=") {
-            return Token(begin: stream.index, type: .notEquals, end: stream.skip(2))
-        } else if stream.peek("<<") {
-            return Token(begin: stream.index, type: .leftShift, end: stream.skip(2))
-        } else if stream.peek(">>") {
-            return Token(begin: stream.index, type: .rightShift, end: stream.skip(2))
-        } else if stream.peek("<=") {
-            return Token(begin: stream.index, type: .lessOrEqual, end: stream.skip(2))
-        } else if stream.peek("<") {
-            return Token(begin: stream.index, type: .less, end: stream.skip())
-        } else if stream.peek(">=") {
-            return Token(begin: stream.index, type: .greaterOrEqual, end: stream.skip(2))
-        } else if stream.peek(">") {
-            return Token(begin: stream.index, type: .greater, end: stream.skip())
-        } else if stream.peek("||") {
-            return Token(begin: stream.index, type: .not, end: stream.skip(2))
-        } else if stream.peek("&&") {
-            return Token(begin: stream.index, type: .and, end: stream.skip(2))
-        } else if stream.peek("!") {
-            return Token(begin: stream.index, type: .not, end: stream.skip())
-        } else if stream.peek("=") {
-            return Token(begin: stream.index, type: .assignment, end: stream.skip())
-        } else if stream.peek("->") {
-            return Token(begin: stream.index, type: .arrow, end: stream.skip(2))
-        } else if stream.peek("|->") {
-            return Token(begin: stream.index, type: .pArrow, end: stream.skip(3))
-        } else if stream.peek("&") {
-            return Token(begin: stream.index, type: .ampersand, end: stream.skip())
-        } else if stream.peek("|") {
-            return Token(begin: stream.index, type: .pipe, end: stream.skip())
-        } else if stream.peek("??") {
-            return Token(begin: stream.index, type: .doubleQuestion, end: stream.skip(2))
-        } else if stream.peek("?") {
-            return Token(begin: stream.index, type: .question, end: stream.skip())
-        } else if stream.peek("+=") {
-            return Token(begin: stream.index, type: .assignmentPlus, end: stream.skip(2))
-        } else if stream.peek("-=") {
-            return Token(begin: stream.index, type: .assignmentMinus, end: stream.skip(2))
-        } else if stream.peek("*=") {
-            return Token(begin: stream.index, type: .assignmentStar, end: stream.skip(2))
-        } else if stream.peek("/=") {
-            return Token(begin: stream.index, type: .assignmentSlash, end: stream.skip(2))
-        } else if stream.peek("%=") {
-            return Token(begin: stream.index, type: .assignmentPercent, end: stream.skip(2))
-        } else if stream.peek("++") {
-            return Token(begin: stream.index, type: .increment, end: stream.skip(2))
-        } else if stream.peek("--") {
-            return Token(begin: stream.index, type: .decrement, end: stream.skip(2))
-        } else if stream.peek("+") {
-            return Token(begin: stream.index, type: .plus, end: stream.skip())
-        } else if stream.peek("-") {
-            return Token(begin: stream.index, type: .minus, end: stream.skip())
-        } else if stream.peek("*") {
-            return Token(begin: stream.index, type: .star, end: stream.skip())
-        } else if stream.peek("/") {
-            return Token(begin: stream.index, type: .slash, end: stream.skip())
-        } else if stream.peek("%") {
-            return Token(begin: stream.index, type: .percent, end: stream.skip())
-        } else if stream.peek("\"") {
-            return Token(begin: stream.index, type: .string, payload: readTill("\""), end: stream.index)
-        } else if stream.peek("'") {
-            return Token(begin: stream.index, type: .character, payload: readTill("'"), end: stream.index)
-        } else if stream.peek("#'") {
-            return Token(begin: stream.index, type: .symbol, payload: readTill("'", skipping: 2), end: stream.index)
-        } else if stream.peek("#:") {
-            return Token(begin: stream.index, type: .symbol, payload: readSymbol(), end: stream.index)
         }
+        else if stream.peek("(")   { return Token(begin: stream.index, type: .leftParen,         end: stream.skip())  }
+        else if stream.peek(")")   { return Token(begin: stream.index, type: .rightParen,        end: stream.skip())  }
+        else if stream.peek("[")   { return Token(begin: stream.index, type: .leftBrack,         end: stream.skip())  }
+        else if stream.peek("]")   { return Token(begin: stream.index, type: .rightBrack,        end: stream.skip())  }
+        else if stream.peek("{")   { return Token(begin: stream.index, type: .leftCurly,         end: stream.skip())  }
+        else if stream.peek("}")   { return Token(begin: stream.index, type: .rightCurly,        end: stream.skip())  }
+        else if stream.peek("...") { return Token(begin: stream.index, type: .ellipsis,          end: stream.skip(3)) }
+        else if stream.peek(".")   { return Token(begin: stream.index, type: .dot,               end: stream.skip())  }
+        else if stream.peek(",")   { return Token(begin: stream.index, type: .comma,             end: stream.skip())  }
+        else if stream.peek("::")  { return Token(begin: stream.index, type: .scope,             end: stream.skip(2)) }
+        else if stream.peek(":")   { return Token(begin: stream.index, type: .colon,             end: stream.skip())  }
+        else if stream.peek(";")   { return Token(begin: stream.index, type: .semicolon,         end: stream.skip())  }
+        else if stream.peek("==")  { return Token(begin: stream.index, type: .equals,            end: stream.skip(2)) }
+        else if stream.peek("!=")  { return Token(begin: stream.index, type: .notEquals,         end: stream.skip(2)) }
+        else if stream.peek("<<")  { return Token(begin: stream.index, type: .leftShift,         end: stream.skip(2)) }
+        else if stream.peek(">>")  { return Token(begin: stream.index, type: .rightShift,        end: stream.skip(2)) }
+        else if stream.peek("<=")  { return Token(begin: stream.index, type: .lessOrEqual,       end: stream.skip(2)) }
+        else if stream.peek("<")   { return Token(begin: stream.index, type: .less,              end: stream.skip())  }
+        else if stream.peek(">=")  { return Token(begin: stream.index, type: .greaterOrEqual,    end: stream.skip(2)) }
+        else if stream.peek(">")   { return Token(begin: stream.index, type: .greater,           end: stream.skip())  }
+        else if stream.peek("||")  { return Token(begin: stream.index, type: .not,               end: stream.skip(2)) }
+        else if stream.peek("&&")  { return Token(begin: stream.index, type: .and,               end: stream.skip(2)) }
+        else if stream.peek("!")   { return Token(begin: stream.index, type: .not,               end: stream.skip())  }
+        else if stream.peek("=")   { return Token(begin: stream.index, type: .assignment,        end: stream.skip())  }
+        else if stream.peek("->")  { return Token(begin: stream.index, type: .arrow,             end: stream.skip(2)) }
+        else if stream.peek("|->") { return Token(begin: stream.index, type: .pArrow,            end: stream.skip(3)) }
+        else if stream.peek("&")   { return Token(begin: stream.index, type: .ampersand,         end: stream.skip())  }
+        else if stream.peek("|")   { return Token(begin: stream.index, type: .pipe,              end: stream.skip())  }
+        else if stream.peek("??")  { return Token(begin: stream.index, type: .doubleQuestion,    end: stream.skip(2)) }
+        else if stream.peek("?")   { return Token(begin: stream.index, type: .question,          end: stream.skip())  }
+        else if stream.peek("+=")  { return Token(begin: stream.index, type: .assignmentPlus,    end: stream.skip(2)) }
+        else if stream.peek("-=")  { return Token(begin: stream.index, type: .assignmentMinus,   end: stream.skip(2)) }
+        else if stream.peek("*=")  { return Token(begin: stream.index, type: .assignmentStar,    end: stream.skip(2)) }
+        else if stream.peek("/=")  { return Token(begin: stream.index, type: .assignmentSlash,   end: stream.skip(2)) }
+        else if stream.peek("%=")  { return Token(begin: stream.index, type: .assignmentPercent, end: stream.skip(2)) }
+        else if stream.peek("++")  { return Token(begin: stream.index, type: .increment,         end: stream.skip(2)) }
+        else if stream.peek("--")  { return Token(begin: stream.index, type: .decrement,         end: stream.skip(2)) }
+        else if stream.peek("+")   { return Token(begin: stream.index, type: .plus,              end: stream.skip())  }
+        else if stream.peek("-")   { return Token(begin: stream.index, type: .minus,             end: stream.skip())  }
+        else if stream.peek("*")   { return Token(begin: stream.index, type: .star,              end: stream.skip())  }
+        else if stream.peek("/")   { return Token(begin: stream.index, type: .slash,             end: stream.skip())  }
+        else if stream.peek("%")   { return Token(begin: stream.index, type: .percent,           end: stream.skip())  }
+        else if stream.peek("\"")  { return Token(begin: stream.index, type: .string,    payload: readTill("\""),             end: stream.index) }
+        else if stream.peek("'")   { return Token(begin: stream.index, type: .character, payload: readTill("'"),              end: stream.index) }
+        else if stream.peek("#'")  { return Token(begin: stream.index, type: .symbol,    payload: readTill("'", skipping: 2), end: stream.index) }
+        else if stream.peek("#:")  { return Token(begin: stream.index, type: .symbol,    payload: readSymbol(),               end: stream.index) }
         return nextWord()
     }
     
