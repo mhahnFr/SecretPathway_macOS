9f02041061a3f75ac9959e4dc91637d0fa3978ae
mhahnFr <83553794+mhahnFr@users.noreply.github.com>
2023-03-29 19:52:20 +0200

    Added ASTFunctionCall

---
+++ src/core/parser/ast/ASTFunctionCall.swift
@@ -0,0 +1,64 @@
+/*
+ * SecretPathway_macOS - A MUD client, for macOS.
+ *
+ * Copyright (C) 2023  mhahnFr
+ *
+ * This file is part of the SecretPathway_macOS. This program is free
+ * software: you can redistribute it and/or modify it under the terms
+ * of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program, see the file LICENSE.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+/// This class represents a function call as an AST node.
+class ASTFunctionCall: ASTExpression {
+    /// The name of the called function.
+    let name: ASTExpression
+    /// The expressions whose result are passed as parameters.
+    let arguments: [ASTExpression]
+    
+    /// Constructs this function call AST node using the given
+    /// name, arguments and the given end position.
+    ///
+    /// - Parameter name: The name.
+    /// - Parameter arguments: The argument expressions.
+    /// - Parameter end: The end position.
+    init(name:      ASTExpression,
+         arguments: [ASTExpression],
+         end:       Int) {
+        self.name      = name
+        self.arguments = arguments
+        
+        super.init(begin: name.begin, end: end, type: .FUNCTION_CALL)
+    }
+    
+    override func describe(_ indentation: Int) -> String {
+        var buffer = "\(super.describe(indentation)) function name:\n" +
+        "\(name.describe(indentation + 4))\n" +
+        "\(String(repeating: " ", count: indentation))arguments:\n"
+        
+        for argument in arguments {
+            buffer.append("\(argument.describe(indentation + 4))\n")
+        }
+        
+        return buffer
+    }
+    
+    override func visit(_ visitor: ASTVisitor) {
+        if visitor.maybeVisit(self) {
+            name.visit(visitor)
+            
+            for argument in arguments {
+                argument.visit(visitor)
+            }
+        }
+    }
+}
