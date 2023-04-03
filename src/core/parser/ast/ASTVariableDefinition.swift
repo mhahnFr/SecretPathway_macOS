2dbab4b699ad5a8c723845de3f86af990c913d59
mhahnFr <83553794+mhahnFr@users.noreply.github.com>
2023-04-03 17:53:47 +0200

    Added ASTVariableDefinition

---
+++ src/core/parser/ast/ASTVariableDefinition.swift
@@ -0,0 +1,70 @@
+/*
+ * SecretPathway_macOS - A MUD client, for macOS.
+ *
+ * Copyright (C) 2023  mhahnFr
+ *
+ * This file is part of the SecretPathway_macOS. This program is free
+ * software: you can redistribute it and/or modify it under the terms
+ * of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program, see the file LICENSE.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+/// This class represents a variable definition as an AST node.
+class ASTVariableDefinition: ASTExpression {
+    /// The modifiers of this variable.
+    let modifiers: [ASTExpression]
+    /// The type of this variable.
+    let returnType: ASTExpression
+    /// The name of this variable.
+    let name: ASTExpression
+    
+    /// Constucts this AST node for variable definitions using the given information.
+    ///
+    /// - Parameter begin: The beginning position.
+    /// - Parameter end: The end position.
+    /// - Parameter modifiers: The declared modifiers of this variable.
+    /// - Parameter type: The declared type of this variable.
+    /// - Parameter name: The declared type of this variable.
+    init(begin:     Int,
+         end:       Int,
+         modifiers: [ASTExpression],
+         type:      ASTExpression,
+         name:      ASTExpression) {
+        self.modifiers  = modifiers
+        self.returnType = type
+        self.name       = name
+        
+        super.init(begin: begin, end: end, type: .VARIABLE_DEFINITION)
+    }
+    
+    override func describe(_ indentation: Int) -> String {
+        var buffer = "\(super.describe(indentation)) modifiers:\n"
+        
+        modifiers.forEach { buffer.append("\($0.describe(indentation + 4))\n") }
+        
+        let indent = String(repeating: " ", count: indentation)
+        buffer.append("\(indent)type:\n")
+        buffer.append("\(returnType.describe(indentation + 4))\n")
+        buffer.append("\(indent)name:\n")
+        buffer.append("\(name.describe(indentation + 4))")
+        
+        return buffer
+    }
+    
+    override func visit(_ visitor: ASTVisitor) {
+        if visitor.maybeVisit(self) {
+            modifiers.forEach { $0.visit(visitor) }
+            returnType.visit(visitor)
+            name.visit(visitor)
+        }
+    }
+}
