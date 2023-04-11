27a0ad90084fe4d579438652ed56e7d21f275b6e
mhahnFr <83553794+mhahnFr@users.noreply.github.com>
2023-04-11 20:04:12 +0200

    Added ASTSymbol

---
+++ src/core/parser/ast/ASTSymbol.swift
@@ -0,0 +1,38 @@
+/*
+ * SecretPathway_macOS - A MUD client, for macOS.
+ *
+ * Copyright (C) 2023  mhahnFr
+ *
+ * This file is part of the SecretPathway_macOS. This program is free
+ * software: you can redistribute it and/or modify it under the terms
+ * of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program, see the file LICENSE.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+/// This class represents a symbol expression as an AST node.
+class ASTSymbol: ASTExpression {
+    /// The represented symbol's name.
+    let symbolName: String
+    
+    /// Constructs this AST node using the given token.
+    ///
+    /// - Parameter token: The token to be represented by this AST node.
+    init(token: Token) {
+        self.symbolName = token.payload! as! String
+        
+        super.init(begin: token.begin, end: token.end, type: .AST_SYMBOL)
+    }
+    
+    override func describe(_ indentation: Int) -> String {
+        super.describe(indentation) + " \"\(symbolName)\""
+    }
+}
