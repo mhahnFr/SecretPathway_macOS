7775adddfd090116ccee5ba1c2e2e15dcd2bdc40
mhahnFr <83553794+mhahnFr@users.noreply.github.com>
2023-04-19 15:52:54 +0200

    Fully implemented FunctionReferenceType

--- src/core/lpc/parser/ast/type/FunctionReferenceType.swift
+++ src/core/lpc/parser/ast/type/FunctionReferenceType.swift
@@ -18,16 +18,46 @@
  * this program, see the file LICENSE.  If not, see <https://www.gnu.org/licenses/>.
  */
 
+/// This class represents a function reference type as an AST node.
 class FunctionReferenceType: AbstractType {
-    let returnType: AbstractType
-    let parameterTypes: [AbstractType]
+    /// The return type of the referenced function.
+    let returnType: ASTExpression
+    /// The types of the parameters of the referenced function.
+    let parameterTypes: [ASTExpression]
+    /// Indicates whether the referenced function has variadic parameters.
     let variadic: Bool
     
-    init(returnType: AbstractType, parameterTypes: [AbstractType], variadic: Bool = false, end: Int) {
+    /// Constructs this AST node using the given information.
+    ///
+    /// - Parameter returnType: The return type of the referenced function.
+    /// - Parameter parameterTypes: The types of the parameters of the referenced function.
+    /// - Parameter variadic: Indicates whether the referenced function has variadic arguments.
+    /// - Parameter end: The end position.
+    init(returnType: ASTExpression, parameterTypes: [ASTExpression], variadic: Bool = false, end: Int) {
         self.returnType = returnType
         self.parameterTypes = parameterTypes
         self.variadic = variadic
         
         super.init(begin: returnType.begin, end: end, type: .FUNCTION_REFERENCE)
+    }
+    
+    override func describe(_ indentation: Int) -> String {
+        let indent = String(repeating: " ", count: indentation)
+        
+        var buffer = "\(super.describe(indentation)) return type:\n\(returnType.describe(indentation + 4))\n" +
+                     "\(indent)parameter types:\n"
+        
+        parameterTypes.forEach { buffer.append("\($0.describe(indentation + 4))\n") }
+        
+        buffer.append("\(indent)variadic: \(variadic)")
+        
+        return buffer
+    }
+    
+    override func visit(_ visitor: ASTVisitor) {
+        if visitor.maybeVisit(self) {
+            returnType.visit(visitor)
+            parameterTypes.forEach { $0.visit(visitor) }
+        }
     }
 }
