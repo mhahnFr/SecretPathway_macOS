a07eace86f043380a409052f45bf33063019f30f
mhahnFr <83553794+mhahnFr@users.noreply.github.com>
2023-04-21 18:56:07 +0200

    Implemented basic interpretation context

--- src/core/lpc/interpreter/Context.swift
+++ src/core/lpc/interpreter/Context.swift
@@ -18,6 +18,37 @@
  * this program, see the file LICENSE.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-struct Context {
+/// This class represents an interpretation context.
+class Context: Instruction {
+    let begin: Int
+    let returnType: TypeProto = InterpreterType.void
+    /// The optional parent context.
+    let parent: Context?
     
+    /// The instruction contained in this context.
+    private(set) var instructions: [Int: Instruction] = [:]
+    
+    var end = 0
+    
+    /// Constructs this context using the optional beginning position
+    /// and the optional parent context.
+    ///
+    /// - Parameters:
+    ///   - begin: The beginning position.
+    ///   - parent: The parent context.
+    init(begin: Int = 0, parent: Context? = nil) {
+        self.begin  = begin
+        self.parent = parent
+    }
+    
+    /// Adds a named identifier to this context.
+    ///
+    /// - Parameters:
+    ///   - begin: The beginning position of the identifier.
+    ///   - name: The name of the identifier.
+    ///   - type: The return type of the identifier.
+    ///   - kind: The AST type of the identifier.
+    func addIdentifier(begin: Int, name: String, type: TypeProto, _ kind: ASTType) {
+        instructions[begin] = Definition(begin: begin, returnType: type, name: name, kind: kind)
+    }
 }
