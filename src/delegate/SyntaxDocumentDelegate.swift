5cfdfaac93ee725ca14adcf16ce4a3f697e61f61
mhahnFr <83553794+mhahnFr@users.noreply.github.com>
2023-05-05 18:06:24 +0200

    Implemented more autoclaving for the editor

--- src/delegate/SyntaxDocumentDelegate.swift
+++ src/delegate/SyntaxDocumentDelegate.swift
@@ -27,17 +27,32 @@ class SyntaxDocumentDelegate: NSObject, NSTextStorageDelegate {
         if editedRange.length == 0 { /* TODO: Handle deletions */ }
         
         let str = textStorage.attributedSubstring(from: editedRange).string
+        let underlying = textStorage.string
         switch str {
         case "\t": textStorage.replaceCharacters(in: editedRange, with: "    ")
             
         case "(", "{", "[", "\"", "'":
-            if isSpecial(textStorage.string, editedRange.location + editedRange.length) {
+            if isSpecial(underlying, editedRange.location + editedRange.length) {
                 textStorage.insert(NSAttributedString(string: getClosingString(str)), at: editedRange.location + editedRange.length)
-                // TODO: Ignore addition
+                // TODO: Ignore addition, move cursor
+            }
+        case "!":
+            if editedRange.location >= 2,
+               underlying[underlying.index(underlying.startIndex, offsetBy: editedRange.location - 2) ..< underlying.index(underlying.startIndex, offsetBy: editedRange.location)] == "/*",
+               isWhitespace(underlying, editedRange.location + editedRange.length) {
+                textStorage.insert(NSAttributedString(string: "!*/"), at: editedRange.location + editedRange.length)
+                // TODO: move cursor
             }
             
         default: break
         }
+    }
+    
+    private func isWhitespace(_ string: String, _ location: Int) -> Bool {
+        guard location > 0,
+              location < string.count else { return true }
+        
+        return string[string.index(string.startIndex, offsetBy: location)].isWhitespace
     }
     
     private func isSpecial(_ string: String, _ location: Int) -> Bool {
