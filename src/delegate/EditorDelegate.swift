0ead6fb2509e07969c55ee8f4a45a438d0ba6ec9
mhahnFr <83553794+mhahnFr@users.noreply.github.com>
2023-05-15 19:01:28 +0200

    Added `<>` as brackets

--- src/delegate/EditorDelegate.swift
+++ src/delegate/EditorDelegate.swift
@@ -184,7 +184,7 @@ class EditorDelegate: NSObject, TextViewBridgeDelegate, NSTextStorageDelegate, N
             textStorage.replaceCharacters(in: editedRange, with: "    ")
             self.delta = 0
             
-        case "(", "{", "[", "\"", "'":
+        case "(", "{", "[", "\"", "'", "<":
             if isSpecial(underlying, editedRange.location + editedRange.length) {
                 let closing = getClosingString(str)
                 textStorage.insert(NSAttributedString(string: closing), at: editedRange.location + editedRange.length)
@@ -385,7 +385,8 @@ class EditorDelegate: NSObject, TextViewBridgeDelegate, NSTextStorageDelegate, N
         let c = string[string.index(string.startIndex, offsetBy: offset - 1)]
         return c == "(" ||
                c == "{" ||
-               c == "["
+               c == "[" ||
+               c == "<"
     }
     
     /// Returns whether the character at the given position is a closing parenthesis.
@@ -400,7 +401,8 @@ class EditorDelegate: NSObject, TextViewBridgeDelegate, NSTextStorageDelegate, N
         let c = string[string.index(string.startIndex, offsetBy: offset)]
         return c == ")" ||
                c == "}" ||
-               c == "]"
+               c == "]" ||
+               c == ">"
     }
     
     /// Returns whether only spaces preceed the given position in the given string.
@@ -480,6 +482,7 @@ class EditorDelegate: NSObject, TextViewBridgeDelegate, NSTextStorageDelegate, N
         case "(": toReturn = ")"
         case "{": toReturn = "}"
         case "[": toReturn = "]"
+        case "<": toReturn = ">"
             
         default:  toReturn = opening
         }
