1cdef619ef59ad54c5d1e10bd4f40d69c63e156b
mhahnFr <83553794+mhahnFr@users.noreply.github.com>
2023-05-25 14:17:25 +0200

    Added ValueSuggestion, added the other suggestions to the project

---
+++ src/gui/editor/suggestions/ValueSuggestion.swift
@@ -0,0 +1,35 @@
+/*
+ * SecretPathway_macOS - A MUD client, for macOS.
+ *
+ * Copyright (C) 2023  mhahnFr
+ *
+ * This file is part of the SecretPathway_macOS. This program is free
+ * software: you can redistribute it and/or modify it under the terms
+ * of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program, see the file LICENSE.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+/// Represents a value as a suggestion.
+struct ValueSuggestion: Suggestion {
+    let suggestion: String
+    
+    /// Initializes this suggestion using the given value.
+    ///
+    /// - Parameter value: The actual value to be suggested.
+    init(value: Any) {
+        if let v = value as? TokenType {
+            suggestion = "\(TypeHelper.tokenTypeString(v))"
+        } else {
+            suggestion = "\(value)"
+        }
+    }
+}
