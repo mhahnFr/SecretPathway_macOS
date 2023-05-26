cb1747be73a5dda728117b69286c45fde1b9c40f
mhahnFr <83553794+mhahnFr@users.noreply.github.com>
2023-05-26 19:04:50 +0200

    Implemented first part of the suggestion visitor

--- src/gui/editor/suggestions/SuggestionVisitor.swift
+++ src/gui/editor/suggestions/SuggestionVisitor.swift
@@ -22,7 +22,106 @@ struct SuggestionVisitor {
     private(set) var suggestionType = SuggestionType.any
     private(set) var expectedType: TypeProto?
     
-    func visit(node: ASTExpression, position: Int, context: Context) {
-        // TODO: Implement        
+    private var position = -1
+    private var lastVisited: ASTExpression?
+    
+    mutating func visit(node: ASTExpression, position: Int, context: Context) -> SuggestionType {
+        guard self.position    !=  position,
+              self.lastVisited !== node
+        else { return suggestionType }
+        
+        self.position    = position
+        self.lastVisited = node
+        expectedType     = nil
+        suggestionType   = visitImpl(node: node, position: position, context: context)
+        
+        return suggestionType
+    }
+    
+    mutating func visitImpl(node: ASTExpression, position: Int, context: Context) -> SuggestionType {
+        switch node.type {
+        case .FUNCTION_DEFINITION:
+            let fun = node as! ASTFunctionDefinition
+            if let last = fun.modifiers.last,
+               position <= last.end {
+                return .typeModifier
+            } else if position <= fun.returnType.end {
+                return .typeModifier
+            } else if position <= fun.name.end {
+                return .literal
+            } else if let last = fun.parameters.last,
+                      position <= last.end {
+                for parameter in fun.parameters {
+                    if position >= parameter.begin,
+                       position <= parameter.end {
+                        return visitImpl(node: parameter, position: position, context: context)
+                    }
+                }
+            } else {
+                return visitImpl(node: fun.body, position: position, context: context)
+            }
+            
+        case .VARIABLE_DEFINITION:
+            let variable = node as! ASTFunctionDefinition
+            
+            // TODO: isOnSameLine
+            if let begin = variable.modifiers.first, position >= begin.begin,
+               let end   = variable.modifiers.last,  position <= end.end {
+                return .typeModifier
+            } else if position >= variable.returnType.begin,
+                      position <= variable.returnType.end {
+                return .typeModifier
+            } else if position >= variable.name.begin,
+                      position <= variable.name.end {
+                return .literal
+            }
+            
+        case .AST_INHERITANCE, .AST_INCLUDE: return .literal
+            
+        case .PARAMETER: return position <= (node as! ASTParameter).declaredType.end ? .type : .literal
+            
+        // TODO: .FUNC_CALL, .OPERATION, .UNARY_OPERATION?
+//        case .AST_RETURN:
+//            let ret = node as! ASTReturn
+//
+//            if let fun = context.queryEnclosingFunction(at: position) {
+//                expectedType = fun.returnType
+//            }
+//            if let returned = ret.returned,
+//               position >= returned.begin,
+//               position <= returned.end {
+//                return returned.hasSubExpressions ? visitImpl(node: returned, position: position, context: context)
+//                                                  : .literalIdentifier
+//            }
+            
+//        case .CAST:
+//            let c = node as! ASTCast
+//
+//            if position <= c.castType.end {
+//                return .type
+//            }
+//            expectedType = cast(TypeProto.self, c.castType)
+//            return .literalIdentifier
+            
+//        case .MISSING, .WRONG:
+//            let hole = node as! ASTHole
+//
+//            if hole.expected == .NAME {
+//                return .literal
+//            } else if hole.expected == .TYPE {
+//                return .type
+//            } else if hole.expected == .IDENTIFIER {
+//                return .identifier
+//            }
+            
+        default: break
+//            for subNode in node.subExpressions {
+//                if position >= subNode.begin,
+//                   position <= subNode.end {
+//                    return visitImpl(node: subNode, position: position, context: context)
+//                }
+//            }
+        }
+        return .any
     }
 }
